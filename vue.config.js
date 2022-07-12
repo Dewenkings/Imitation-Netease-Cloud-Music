bcc29ca6f03e902855fc04fe591a6743687bd1a2
“学无止境” <1278962628@qq.com>
2022-07-12 19:30:50 +0800

    环境搭建与CDN配置

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,31 @@ module.exports = {
       }
     }
   },
+  // 自定义打包入口
+  chainWebpack: config => {
+    // 发布模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        'element-ui': 'ELEMENT'
+      })
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
+    })
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+    config.plugin('html').tap(args => {
+      args[0].isProd = false
+      return args
+    })
+  },
   devServer: {
     proxy: {
       '/api': {
